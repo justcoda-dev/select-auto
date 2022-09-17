9f48c4e1845a805ccbdfc21131f793ca029a2d11
oleksandr.koval <koval.develop@gmail.com>
2022-09-17 14:25:20 +0300

    init

---
+++ src/components/Container/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container"
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+margin: 100px;
+}
+</style>
